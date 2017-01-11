b08ece90cb8ed24ef41884a14c1108a67af3a80a
Eric Dao <ericdao1610@gmail.com>
2017-01-11 13:20:35 -0500

    Second commit - Jan 11 2017

---
+++ ProjectWithGit/src/GitLocal.java
@@ -0,0 +1,12 @@
+
+public class GitLocal {
+
+	public GitLocal() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main (String [] args){
+		System.out.println ("Fun with Git!");
+	}
+
+}
