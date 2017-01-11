874be3a228fbeab8b4fec1c18002c84295518d36
cimcim <cimcim@MSI>
2017-01-11 13:35:10 -0500

    hihi

--- ProjectWithGit/src/GitLocal.java
+++ ProjectWithGit/src/GitLocal.java
@@ -5,8 +5,19 @@ public class GitLocal {
 		// TODO Auto-generated constructor stub
 	}
 	
+	//public neonisshit(){
+		//ffffffffffff
+	//}
+	
 	public static void main (String [] args){
 		System.out.println ("Fun with Git!");
+		int a = 10;
+		System.out.println(a);
 	}
-
+ 
+	
+	
+	//https://github.com/eric1610/EECS_2311
+		//https://github.com/eric1610/EECS_2311
+			
 }
---
+++ ProjectWithGit/src/Hellololololo.java
@@ -0,0 +1,10 @@
+
+public class Hellololololo {
+
+	public Hellololololo () {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main (String [] args){
+		System.out.println ("HELEOWOEFWOJEFIWF");
+	}
+}
