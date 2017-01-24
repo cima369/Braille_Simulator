794c9ab021cb6159f635d27e7129c8e0918c667f
Eric Dao <ericdao1610@gmail.com>
2017-01-23 23:22:47 -0500

    Display class using JavaFX, full features have not been implemented yet

--- ProjectWithMVC/eecs2311simulator/eecs2311simulator/Display.java
+++ ProjectWithMVC/eecs2311simulator/eecs2311simulator/Display.java
@@ -32,7 +32,7 @@ public class Display extends Application {
 	 
 	            @Override
 	            public void handle(ActionEvent event) {
-	             //   System.out.println("Hello World Today World!");
+	             //   System.out.println("Hello World Today Wild!");
 	            	text.setText("Wrong");
 	            }
 	        });
