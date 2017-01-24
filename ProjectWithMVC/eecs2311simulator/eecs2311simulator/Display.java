8ec55ab708d9c7a5db6adfa1729dfb2f72fbb509
Eric Dao <ericdao1610@gmail.com>
2017-01-23 23:17:37 -0500

    Starting display using Javafx, have yet to implement actual features

--- ProjectWithMVC/eecs2311simulator/eecs2311simulator/Display.java
+++ ProjectWithMVC/eecs2311simulator/eecs2311simulator/Display.java
@@ -32,7 +32,7 @@ public class Display extends Application {
 	 
 	            @Override
 	            public void handle(ActionEvent event) {
-	             //   System.out.println("Hello World!");
+	             //   System.out.println("Hello World Today!");
 	            	text.setText("Wrong");
 	            }
 	        });
