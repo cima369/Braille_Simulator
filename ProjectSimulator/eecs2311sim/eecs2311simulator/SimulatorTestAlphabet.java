86703640dad41aa86f059fb4c87f8970ce231e82
cimcim <cimcim@192.168.0.23>
2017-02-05 17:25:19 -0500

    seperated

--- ProjectSimulator/eecs2311sim/eecs2311simulator/SimulatorTestAlphabet.java
+++ ProjectSimulator/eecs2311sim/eecs2311simulator/SimulatorTestAlphabet.java
@@ -15,7 +15,7 @@ public class SimulatorTestAlphabet {
 	}
 
 	@Test
-	public void testsetCharToBraillePinsAtoM() {
+	public void testsetCharToBraillePinsAtoM(){
 	
 	panel.setCharToBraillePins(0, 'a');
 	assertEquals(panel.checkPinRaised(0, 0), true);
@@ -149,7 +149,7 @@ public class SimulatorTestAlphabet {
 	}
 	
 	@Test
-	public void testsetCharToBraillePinsNtoZ() {
+	public void testsetCharToBraillePinsNtoZ(){
 		
 	panel.setCharToBraillePins(3, 'n');
 	assertEquals(panel.checkPinRaised(3, 0), false);
